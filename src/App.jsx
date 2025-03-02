0afb33762084845392d7817bc9504bc34a79082d
hasanhameed <hasanhameed858@gmail.com>
2025-03-02 21:14:21 +0530

    Netflix clone bulding

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MovieCard from './Components/MovieCard'
-const movies = [
-  {
-    id:"1",
-    title: "Inception",
-    release_date: "2010-07-16",
-    url: "https://via.placeholder.com/300x450?text=Inception",
-  },
-  {
-    id:"2",
-    title: "Interstellar",
-    release_date: "2014-11-07",
-    url: "https://via.placeholder.com/300x450?text=Interstellar",
-  },
-  {
-    id:"2",
-    title: "The Dark Knight",
-    release_date: "2008-07-18",
-    url: "https://via.placeholder.com/300x450?text=The+Dark+Knight",
-  },
-];
+import React from 'react'
 
-function App() {
-
- return (
-    <>
-     {movies.map((movie,index)=>(<MovieCard  key={index} movie={movie}/>))}
-    </>
-  
+const App = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
   )
 }
-
 
 export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./Navbar.css"
+import logo from "../src/assets/netflixlogo.png"
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            navBar
+        </div>
+    )
+}
+
+export default Navbar;
